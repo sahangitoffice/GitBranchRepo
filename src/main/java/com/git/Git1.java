de66a337e478847ddc6cb803259b66cca76cc10c
sahangitoffice <sahan@hsenidlanka.com>
2016-09-28 12:26:04 +0530

    add modifications to remote

--- src/main/java/com/git/Git1.java
+++ src/main/java/com/git/Git1.java
@@ -6,5 +6,8 @@ package com.git;
 public class Git1 {
     public static void main(String[] args) {
         System.out.println("go");
+        System.out.println("gogo");
+        System.out.println("gogogo");
+        System.out.println("gogogo");
     }
 }
